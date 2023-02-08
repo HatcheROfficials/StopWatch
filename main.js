f550b35749bd61a4b5dbc395e5d65ddfc03fbee4
HatcheROfficials <hatcher.officials@gmail.com>
2023-02-08 23:49:32 +0530

    Implemented Milliseconds. Improved Logic. Modified Timer Position
    Implemented the milliseconds timer. Adjusted the timer position to accommodate milliseconds timer.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var hour = document.getElementById("hour");
 var min = document.getElementById("min");
 var sec = document.getElementById("sec");
+var mill = document.getElementById("milliSec");
 
 var startBtn = document.getElementById("startBtn");
 var stopBtn = document.getElementById("stopBtn");
@@ -9,16 +10,25 @@ var resetBtn = document.getElementById("resetBtn");
 var h="00";
 var m="00"; 
 var s="00";
+var mil="0";
+
 var stopStopWatchID;
 
 function startStopWatch(){
     startBtn.removeEventListener("click",startStopWatch);
-    stopStopWatchID = setInterval(stopWatchCounter,1000);
+    stopStopWatchID = setInterval(stopWatchCounter,100);
 
     function stopWatchCounter(){
-        s++;
+        mil++;
+        if(mil == 10){
+            mil=0;
+            s++;
+            if(s<10){
+                s = "0" + s;
+            }
+        }
         if(s == 60){
-            s=0;
+            s="00";
             m++;
             if(m<10){
                 m = "0" + m;
@@ -27,20 +37,15 @@ function startStopWatch(){
         if(m == 60){
             m=0;
             h++;
-            if(m<10){
-                m = "0" + m;
-            }
             if(h<10){
                 h = "0" + h;
             }
-        }
-        if(s<10){
-            s = "0" + s;
         }
         
         hour.textContent = h;
         min.textContent = m;
         sec.textContent = s;
+        mill.textContent = mil;
     }
 }
 
@@ -53,9 +58,11 @@ function resetStopWatch(){
     h="00";
     m="00";
     s="00";
+    mil=0;
     hour.textContent = h;
     min.textContent = m;
     sec.textContent = s;
+    mill.textContent = mil;
 }
 
 startBtn.addEventListener("click",startStopWatch);
