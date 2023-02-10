66de61202000d00b8abb9e8957e3b45ee3cbd0fd
HatcheROfficials <hatcher.officials@gmail.com>
2023-02-10 22:38:45 +0530

    Implemented Dynamic Timer with User Input
    Implemented timer where user can enter the timer value via drop down menus.
    
    Improved logic and added comments

--- main.js
+++ main.js
@@ -13,8 +13,31 @@ var resetBtn = document.getElementById("resetBtn");
 var timerMode = document.getElementById("timerSelectionBtn");
 var stopWatchMode = document.getElementById("StopwatchSelectionBtn");
 
+// User Time input
+var userInputTimer = document.getElementById("timerInput");
+
+// Initially for stopwatch function, the form will not show
+userInputTimer.style.display = "none";
+
+// taking user input
+var hourInput = document.getElementsByName("formhour");
+var minInput = document.getElementsByName("formminute");
+var secInput = document.getElementsByName("formsecond");
+var btnInput = document.getElementById("timerValueBtn");
+var userInputTime = [];
+
+// user input is stored in array with hour,min and sec at index 0,1,2
+function timerUserInput() {
+    userInputTime[0] = hourInput.item(0).value;
+    userInputTime[1] = minInput.item(0).value;
+    userInputTime[2] = secInput.item(0).value;
+    resetStopWatch();
+}
+
+btnInput.addEventListener("click", timerUserInput);
+
 // Mode flag 
-// 0 - stopwatch
+// 0 - stopwatch 
 // 1 - timer
 modeFlag = 0;
 
@@ -27,12 +50,6 @@ var h = "00";
 var m = "00";
 var s = "00";
 var mil = "0";
-
-// Timer variables
-h = "00";
-m = "00";
-s = "00";
-mil = "0";
 
 // setting initial time
 hour.textContent = h;
@@ -47,15 +64,19 @@ var stopStopWatchID;
 function modeFunction(event) {
     if (event.target == timerMode) {
         modeFlag = 1;
-      
+
         timerMode.classList.remove("inactive");
-        stopWatchMode.classList.add("inactive");       
+        stopWatchMode.classList.add("inactive");
+
+        userInputTimer.style.display = "";
 
     } else if (event.target == stopWatchMode) {
         modeFlag = 0;
 
         timerMode.classList.add("inactive");
-        stopWatchMode.classList.remove("inactive");       
+        stopWatchMode.classList.remove("inactive");
+
+        userInputTimer.style.display = "none";
     }
 
     // initialize watch
@@ -98,8 +119,8 @@ function startStopWatch() {
                     h = "0" + h;
                 }
             }
-        } 
-        
+        }
+
         // Timer Logic
         else if (modeFlag == 1) {
             mil++;
@@ -110,25 +131,25 @@ function startStopWatch() {
                     s = "0" + s;
                 }
             }
-            if(h == 0 && m == 0 && s == 0){
+            if (h == 0 && m == 0 && s == 0) {
                 sec.textContent = "00";
                 mill.textContent = "00";
                 alert("Times Up");
                 stopStopWatch();
                 return;
             }
-            if(s == 0){
-                if(m == 0){
+            if (s == 0) {
+                if (m == 0) {
                     h--;
-                    m=59;
-                    s=60
-                    if(h < 10){
+                    m = 59;
+                    s = 60
+                    if (h < 10) {
                         h = "0" + h;
                     }
-                } else{
+                } else {
                     m--;
-                    s=60;
-                    if(m < 10){
+                    s = 60;
+                    if (m < 10) {
                         m = "0" + m;
                     }
                 }
@@ -159,10 +180,26 @@ function resetStopWatch() {
         s = "00";
         mil = 0;
     } else if (modeFlag == 1) {
-        h = "00";
-        m = "05";
-        s = "00";
+        h = userInputTime[0];
+        m = userInputTime[1];
+        s = userInputTime[2];
         mil = 0;
+
+        if (h == undefined || m == undefined || s == undefined) {
+            h = 0;
+            m = 0;
+            s = 0;
+        }
+
+        if (h < 10) {
+            h = "0" + h;
+        }
+        if (m < 10) {
+            m = "0" + m;
+        }
+        if (s < 10) {
+            s = "0" + s;
+        }
     }
 
     hour.textContent = h;
